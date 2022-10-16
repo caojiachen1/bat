34de3c186b9bf2dd0814df33edf73b5949fafde9
caojiachen1 <31117295+caojiachen1@users.noreply.github.com>
2022-10-16 12:12:12 +0800

    Add files via upload

--- hyper-v.bat
+++ hyper-v.bat
@@ -1,11 +1,6 @@
 pushd "%~dp0"
- 
 dir /b %SystemRoot%\servicing\Packages\*Hyper-V*.mum >hyper-v.txt
- 
 for /f %%i in ('findstr /i . hyper-v.txt 2^>nul') do dism /online /norestart /add-package:"%SystemRoot%\servicing\Packages\%%i"
- 
 del hyper-v.txt
- 
 Dism /online /enable-feature /featurename:Microsoft-Hyper-V-All /LimitAccess /ALL
 
-rem 版权声明：本文为博主原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接和本声明。

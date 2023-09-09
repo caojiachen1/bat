52971772435d8c6471ef2e0be901ae008e30e550
hqsrawmelon <31117295+caojiachen1@users.noreply.github.com>
2023-09-09 20:58:28 +0800

    Add files via upload

---
+++ code-server.sh
@@ -0,0 +1,5 @@
+export PASSWORD="1111"
+wget https://github.com/coder/code-server/releases/download/v4.16.1/code-server-4.16.1-linux-arm64.tar.gz
+tar -xvf ./code-server-4.16.1-linux-arm64.tar.gz
+cd code-server-4.16.1-linux-arm64/bin
+./code-server
